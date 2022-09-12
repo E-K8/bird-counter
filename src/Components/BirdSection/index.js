25757825519e608f6222170a36182d1c92dcb276
E-K8 <ekatplat@gmail.com>
2022-09-12 18:33:40 +0100

    swap plus and minus BirdButton, add individual RESET button

--- src/Components/BirdSection/index.js
+++ src/Components/BirdSection/index.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import { BirdButton } from "../Button";
+import { BirdButton } from '../Button';
 
-import "./index.css";
+import './index.css';
 
-export function BirdSection({image, bird, addTotalBirds, deductTotalBirds}){    
-
+export function BirdSection({ image, bird, addTotalBirds, deductTotalBirds }) {
   const [count, setCount] = useState(0);
   const altText = `Nice photo of a ${bird}`;
 
@@ -16,14 +15,31 @@ export function BirdSection({image, bird, addTotalBirds, deductTotalBirds}){
   function minus() {
     return count === 0 ? null : setCount(count - 1);
   }
-    
-    return(
-        <article className="gallery-item">            
-            <img src={image} alt={altText}></img>
-            <h2> {count < 0 ? 0: count} {count === 1 ? bird : bird + "s"} </h2>
-            <BirdButton className="btn" text="➖" onClick={() => {minus(); if(count>0)deductTotalBirds()}} />
-            <BirdButton className="btn" text="➕" onClick={(e) => {plus(); addTotalBirds()}} />
-        </article>
-        
-    ) 
+
+  return (
+    <article className='gallery-item'>
+      <img src={image} alt={altText}></img>
+      <h2>
+        {' '}
+        {count < 0 ? 0 : count} {count === 1 ? bird : bird + 's'}{' '}
+      </h2>
+      <BirdButton
+        className='btn'
+        text='➕'
+        onClick={(e) => {
+          plus();
+          addTotalBirds();
+        }}
+      />
+      <BirdButton
+        className='btn'
+        text='➖'
+        onClick={() => {
+          minus();
+          if (count > 0) deductTotalBirds();
+        }}
+      />
+      <BirdButton className='btn' text='RESET' onClick={() => setCount(0)} />
+    </article>
+  );
 }
